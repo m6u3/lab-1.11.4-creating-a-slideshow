00e2d5c917c3db3c75c78f7f763c9d15b2803e33
Miguel Rodriguez <mgarl22@gmail.com>
2021-10-20 20:31:52 -0400

    half way there

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+//First I need to select the img Element
+let imgElement = document.querySelector('.image');
+
+// Now I need to select the left and rigth buttons
+ let previousBtn = document.querySelector('.previous');
+ let nextBtn = document.querySelector('.next');
+
+ //lets also get the bubbles
+ let buubles =  document.querySelectorAll('.bubble');
+
+ //I need a variable for the current Image that is displaying
+ let currentImage = 0;
+
+ // Now lets put the first Image as default whe page is loaded
+
+imgElement.src = images[currentImage];
+
+
+//  Lets start with the bubbles menu we need a loop to iterate between them.
+
+for (i = 0; i < buubles.length; i++){
+    //now we need an event listener for each bubble
+    buubles[i].addEventListener('click', function(e){
+        //lets start by changing the text to a filled circle
+        e.target.textContent = '●';
+        currentImage = i;
+        console.log(e.target);
+        console.log(currentImage)
+    })
+}
